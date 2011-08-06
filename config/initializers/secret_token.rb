10c52a055a96eda4d61001745052a8935935528b
John <jnelson11@gmail.com>
2011-08-06 17:02:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jrubyy::Application.config.secret_token = '1655748368579da93caf27c8a80e4e902169cedf51ad79c7e8e6eebbc7badecc5232ad4e83ae77ed41d0da42f314b70cfa5eef0aa5c6f248675bb703feadb906'
